405e2fc58d2659977a1a57c9f008805757f574a4
Tsenda-Djessy-Flavien <djesstsenda@gmail.com>
2023-05-16 12:48:43 +0100

    debugger et fixer les erreurs

---
+++ android/app/src/main/kotlin/com/example/quiz_app_fix_bug/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.quiz_app_fix_bug
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+import 'package:adv_basics/app/quiz.dart';
+
+void main() {
+  runApp(const Quiz());
+}
