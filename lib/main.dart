2f65e58f8a374e8467020bd296f2889eb7823c5a
Tsenda-Djessy-Flavien <djesstsenda@gmail.com>
2023-05-17 11:38:41 +0100

    Travailler avec Flutter DevTools

--- lib/main.dart
+++ lib/main.dart
@@ -5,3 +5,7 @@ import 'package:adv_basics/app/quiz.dart';
 void main() {
   runApp(const Quiz());
 }
+
+// TsendaLAB note
+/// pour debugger votre app flutter, vous pouvez aussi aller sur le menu en haut de VsCode
+/// Affichage -> Platte de commande -> Flutter Open DevTools -> Open DevTools in web Browser 
